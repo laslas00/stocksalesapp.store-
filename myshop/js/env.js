e015599798e3bf069c2216e054c34d7997b00f9f
laslas00 <ifenyiemma9@gmail.com>
2026-05-15 15:11:02 +0000

    Delete myshop directory

--- myshop/js/env.js
+++
@@ -1,8 +0,0 @@
-// js/env.js - Environment configuration
-// These values are replaced by Vercel at build time or fetched at runtime
-
-window.ENV = {
-    MYSHOPSUPABASE_URL: 'https://zexxdoxuzvkovszfqcio.supabase.co',
-    MYSHOPSUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpleHhkb3h1enZrb3ZzemZxY2lvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NjUzMzQ1MSwiZXhwIjoyMDkyMTA5NDUxfQ.yLhpe7k3bxu73az7qqKssN33JGB358hhWqZIUC490Pk'
-};
-
